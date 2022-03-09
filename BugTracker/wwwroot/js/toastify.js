00f72d91a88d7e347c4ea21f158fe43637714b6d
LHermann <l.hermannsauer@gmail.com>
2022-03-09 20:24:36 -0300

    finished implementing assign and unassign users in details role view

---
+++ BugTracker/wwwroot/js/toastify.js
@@ -0,0 +1,32 @@
+﻿// Basic success and error toasts with customizable text
+
+
+// error
+function errorToast(message = "Something went wrong") {
+    Toastify({
+        text: message,
+        duration: 1500,
+        style: {
+            background: "linear-gradient(to right, #B0099B, #C2246D)",
+            cursor: "default",
+        },
+        position: "center",
+        stopOnFocus: false,
+        onClick: null,
+    }).showToast();
+}
+
+// Success
+function successToast(message = "Success") {
+    Toastify({
+        text: message,
+        duration: 1500,
+        style: {
+            background: "linear-gradient(to right, #00B09B, #96C96D)",
+            cursor: "default",
+        },
+        position: "center",
+        stopOnFocus: false,
+        onClick: null,
+    }).showToast();
+}
