e4417098e98c1a036218e3009b191af7e8a88365
LHermann <l.hermannsauer@gmail.com>
2022-03-13 12:24:31 -0300

    started with add people implementation

---
+++ BugTracker/Controllers/ApiControllers/AppUserController.cs
@@ -0,0 +1,36 @@
+﻿namespace BugTracker.Controllers.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppUserController : ControllerBase
+    {
+        private UserManager<AppUser> userManager;
+        public AppUserController(RoleManager<IdentityRole> roleMgr, UserManager<AppUser> userMgr)
+        {
+            userManager = userMgr;
+        }
+
+
+        public async Task<IActionResult> GetUsers([FromQuery] string? filter)
+        {
+            var users = await userManager.Users.ToListAsync();
+
+            if (filter != null)
+                users = users.Where(u =>
+                        (
+                            u.FirstName.ToLower().Contains(filter) ||
+                            u.UserName.ToLower().Contains(filter) ||
+                            u.Email.ToLower().Contains(filter) ||
+                            u.LastName.ToLower().Contains(filter)
+
+                        )).ToList();
+
+
+            if (users == null)
+                return NoContent();
+
+            return Ok(users);
+        }
+
+    }
+}
